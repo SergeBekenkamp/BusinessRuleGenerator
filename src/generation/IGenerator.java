bc0e5c07df6d6e125d4b2448de123dc80917d779
Serge <Serge899@gmail.com>
2013-03-15 14:24:29 +0100

    Generation :)

--- src/generation/IGenerator.java
+++ src/generation/IGenerator.java
@@ -1,5 +1,10 @@
 package generation;
+import java.util.ArrayList;
+
+import domain.BusinessRule;
 
 public interface IGenerator {
+	
+	public void generate(ArrayList<BusinessRule> businessRules) throws GenerationException;
 
 }
--- src/generation/SQLGenerator.java
+++ src/generation/SQLGenerator.java
@@ -1,5 +1,19 @@
 package generation;
 
+import java.util.ArrayList;
+
+import domain.BusinessRule;
+
 public class SQLGenerator implements IGenerator {
 
+	@Override
+	public void generate(ArrayList<BusinessRule> businessRules)	throws GenerationException {
+		for(BusinessRule b : businessRules){
+			
+		}
+		
+	}
+	
+	
+	
 }
